42522e53273fc444bac99e37e195b54ba3c3d923
mcmanusniall <niall.mcmanus@hotmail.com>
2023-02-23 13:03:02 +0000

    Test classes added

---
+++ test/sml/Test.java
@@ -0,0 +1,70 @@
+package sml;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+    @org.junit.jupiter.api.Test
+    public void testFileExists() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testFileDoesNotExists() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testAddTwoRegisters() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSubtractTwoRegisters() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMultiplyTwoRegisters() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDivideTwoRegisters() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testPrintRegisterContents() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMoveIntegerBetweenTwoRegisters() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testContentsOfRegisterEqualZero() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testInstructionStructureIsCorrect() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testLabelFormattingCorrect() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testLabelDefinitionAlreadyStated() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testLabelledInstructionDoesNotExist() {
+
+    }
+}
