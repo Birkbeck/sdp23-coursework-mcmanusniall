f83221027e4c8d5144de69068416cf9359152146
mcmanusniall <niall.mcmanus@hotmail.com>
2023-02-24 11:25:49 +0000

    Added testMoveInstruction();

--- test/sml/Test.java
+++ test/sml/Test.java
@@ -1,8 +1,13 @@
 package sml;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Test {
+
     @org.junit.jupiter.api.Test
     public void testFileExists() {
 
@@ -11,6 +16,15 @@ class Test {
     @org.junit.jupiter.api.Test
     public void testFileDoesNotExists() {
 
+    }
+    @org.junit.jupiter.api.Test
+    public void testMoveInstruction() {
+        Machine m = new Machine(new Registers());
+        Registers.Register registerName = Registers.Register.EAX;
+        Instruction instruction = new sml.instruction.MoveInstruction("", registerName, 2);
+        instruction.execute(m);
+        Registers registers = m.getRegisters();
+        assertEquals(2, registers.get(registerName));
     }
 
     @org.junit.jupiter.api.Test
