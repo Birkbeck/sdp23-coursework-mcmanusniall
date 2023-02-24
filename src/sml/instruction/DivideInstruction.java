3bc31426dd1430acb4f6997f6e9510b97a925cf0
mcmanusniall <niall.mcmanus@hotmail.com>
2023-02-24 12:22:18 +0000

    Added DivideInstruction() class

---
+++ src/sml/instruction/DivideInstruction.java
@@ -0,0 +1,46 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+public class DivideInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "mul";
+
+    public DivideInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        if(value1 == 0 || value2 == 0){
+            System.out.println("Unable to execute. Invalid division by 0.");
+            return NORMAL_PROGRAM_COUNTER_UPDATE + 1;
+        } else {
+            m.getRegisters().set(result, value1 / value2);
+            return NORMAL_PROGRAM_COUNTER_UPDATE;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + source;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals() {
+        return false;
+    }
+}
