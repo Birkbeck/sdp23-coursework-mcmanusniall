10ff5918f3285513e5ed4a1b8549444aa1ef986e
mcmanusniall <niall.mcmanus@hotmail.com>
2023-02-25 10:09:42 +0000

    Added support for JumpIfNotZeroInstruction.execute()

--- src/sml/instruction/JumpIfNotZeroInstruction.java
+++ src/sml/instruction/JumpIfNotZeroInstruction.java
@@ -1,47 +1,48 @@
-//package sml.instruction;
-//
-//import sml.Instruction;
-//import sml.Machine;
-//import sml.RegisterName;
-//
-//// TODO: write a JavaDoc for the class
-//
-//public class JumpIfNotZeroInstruction extends Instruction {
-//    private final RegisterName source;
-//    private final String labelReference;
-//
-//    public static final String OP_CODE = "jnz";
-//
-//    public JumpIfNotZeroInstruction(String label, RegisterName source, String labelReference) {
-//        super(label, OP_CODE);
-//        this.source = source;
-//        this.labelReference = labelReference;
-//    }
-//
-//    @Override
-//    public int execute(Machine m) {
-//
-//        try {
-//            if(m.getRegisters().get(source) == 0);
-//            System.out.println("Unable to execute ");
-//        } catch(Exception e){
-//            e.printStackTrace(System.out);
-//        }
-//        return NORMAL_PROGRAM_COUNTER_UPDATE;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return getLabelString() + getOpcode() + " " + result + " " + value;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public boolean equals() {
-//        return false;
-//    }
-//}
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+// TODO: write a JavaDoc for the class
+
+public class JumpIfNotZeroInstruction extends Instruction {
+    private final RegisterName source;
+    private final String instructionLabel;
+
+    public static final String OP_CODE = "jnz";
+
+    public JumpIfNotZeroInstruction(String label, RegisterName source, String instructionLabel) {
+        super(label, OP_CODE);
+        this.source = source;
+        this.instructionLabel = instructionLabel;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        if(m.getRegisters().get(source) == 0) {
+            System.out.println("Did not execute \"" + this.toString() + "\" as " + source.toString() +
+                                " = 0. Moving to execute next valid command.");
+        }
+        else if(m.getRegisters().get(source) != 0) {
+            Instruction labelledInstruction = m.getProgram().get(m.getLabels().getAddress(instructionLabel));
+            labelledInstruction.execute(m);
+        }
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + source + " " + instructionLabel;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals() {
+        return false;
+    }
+}
