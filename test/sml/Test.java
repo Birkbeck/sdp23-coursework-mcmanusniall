62aec26faf72d1a4d51d2004c393b07664cdf408
mcmanusniall <niall.mcmanus@hotmail.com>
2023-03-03 20:17:04 +0000

    Added toString() tests

--- test/sml/Test.java
+++ test/sml/Test.java
@@ -1,10 +1,28 @@
 package sml;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Test {
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOutput = System.out;
+
+    @BeforeEach
+    public void setUpOutputStream() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void windDownOutputStream() {
+        System.setOut(originalOutput);
+    }
 
     @org.junit.jupiter.api.Test
     public void testFileExists() {
@@ -33,9 +51,7 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 7);
         // Create Add Instruction
-        Instruction addInstruction = new sml.instruction.AddInstruction(
-                "", resultRegisterName, sourceRegisterName
-        );
+        Instruction addInstruction = new sml.instruction.AddInstruction(null, resultRegisterName, sourceRegisterName);
         addInstruction.execute(m);
         // Test
         assertEquals(9, registers.get(resultRegisterName));
@@ -54,9 +70,7 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 5);
         // Create Divide Instruction
-        Instruction divideInstruction = new sml.instruction.DivideInstruction(
-                "", resultRegisterName, sourceRegisterName
-        );
+        Instruction divideInstruction = new sml.instruction.DivideInstruction(null, resultRegisterName, sourceRegisterName);
         divideInstruction.execute(m);
         // Test
         assertEquals(20, registers.get(resultRegisterName));
@@ -75,9 +89,7 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 5);
         // Create Divide Instruction
-        Instruction divideInstruction = new sml.instruction.DivideInstruction(
-                "", resultRegisterName, sourceRegisterName
-        );
+        Instruction divideInstruction = new sml.instruction.DivideInstruction(null, resultRegisterName, sourceRegisterName);
         // Test
         assertThrows(ArithmeticException.class, () -> divideInstruction.execute(m));
     }
@@ -99,17 +111,13 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 5);
         // Create Labelled Instruction
-        Instruction labelledInstruction = new sml.instruction.MoveInstruction(
-                "test", resultRegisterName, 8
-        );
+        Instruction labelledInstruction = new sml.instruction.MoveInstruction("test", resultRegisterName, 8);
         // Add Labelled Instruction to the program.
         m.getProgram().add(labelledInstruction);
         // Add Labelled Instruction to the Labels HashMap.
         m.getLabels().addLabel("test", 0);
         // Create jnz Instruction
-        Instruction jnzInstruction = new sml.instruction.JumpIfNotZeroInstruction(
-                "", sourceRegisterName, "test"
-        );
+        Instruction jnzInstruction = new sml.instruction.JumpIfNotZeroInstruction(null, sourceRegisterName, "test");
         // Test
         jnzInstruction.execute(m);
         assertEquals(8, registers.get(resultRegisterName));
@@ -127,17 +135,13 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 0);
         // Create Labelled Instruction
-        Instruction labelledInstruction = new sml.instruction.MoveInstruction(
-                "test", resultRegisterName, 8
-        );
+        Instruction labelledInstruction = new sml.instruction.MoveInstruction("test", resultRegisterName, 8);
         // Add Labelled Instruction to the program.
         m.getProgram().add(labelledInstruction);
         // Add Labelled Instruction to the Labels HashMap.
         m.getLabels().addLabel("test", 0);
         // Create jnz Instruction
-        Instruction jnzInstruction = new sml.instruction.JumpIfNotZeroInstruction(
-                "", sourceRegisterName, "test"
-        );
+        Instruction jnzInstruction = new sml.instruction.JumpIfNotZeroInstruction(null, sourceRegisterName, "test");
         jnzInstruction.execute(m);
         // Test
         assertEquals(0, registers.get(resultRegisterName));
@@ -154,7 +158,7 @@ class Test {
     public void testMoveInstruction() {
         Machine m = new Machine(new Registers());
         Registers.Register registerName = Registers.Register.EAX;
-        Instruction instruction = new sml.instruction.MoveInstruction("", registerName, 2);
+        Instruction instruction = new sml.instruction.MoveInstruction(null, registerName, 2);
         instruction.execute(m);
         Registers registers = m.getRegisters();
         assertEquals(2, registers.get(registerName));
@@ -173,9 +177,7 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 7);
         // Create Add Instruction
-        Instruction multiplyInstruction = new sml.instruction.MultiplyInstruction(
-                "", resultRegisterName, sourceRegisterName
-        );
+        Instruction multiplyInstruction = new sml.instruction.MultiplyInstruction(null, resultRegisterName, sourceRegisterName);
         multiplyInstruction.execute(m);
         // Test
         assertEquals(70, registers.get(resultRegisterName));
@@ -190,10 +192,11 @@ class Test {
         // Populate result register with Integer.
         Registers.Register sourceRegisterName = Registers.Register.EAX;
         registers.set(sourceRegisterName, 2);
-        // Create Add Instruction
-        Instruction printInstruction = new sml.instruction.PrintInstruction("", sourceRegisterName);
+        // Create Print Instruction
+        Instruction printInstruction = new sml.instruction.PrintInstruction(null, sourceRegisterName);
         printInstruction.execute(m);
-        // TODO: Add assertion.
+        // Test
+        assertEquals("EAX - 2", output.toString().trim());
     }
 
     @org.junit.jupiter.api.Test
@@ -209,9 +212,7 @@ class Test {
         Registers.Register sourceRegisterName = Registers.Register.ECX;
         registers.set(sourceRegisterName, 7);
         // Create Subtract Instruction
-        Instruction subtractInstruction = new sml.instruction.SubtractInstruction(
-                "", resultRegisterName, sourceRegisterName
-        );
+        Instruction subtractInstruction = new sml.instruction.SubtractInstruction(null, resultRegisterName, sourceRegisterName);
         subtractInstruction.execute(m);
         // Test
         assertEquals(-5, registers.get(resultRegisterName));
@@ -219,26 +220,32 @@ class Test {
 
     @org.junit.jupiter.api.Test
     public void testLabelDefinitionAlreadyExists() {
+        // Create a Machine to execute the instructions.
         Machine m = new Machine(new Registers());
+        // Add a label to the Labels object.
         m.getLabels().addLabel("test", 0);
+        // Test
         assertThrows(IllegalArgumentException.class,
-                () -> m.getLabels().addLabel("test", 0));
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testInstructionStructureIsCorrect() {
-        // TODO: test unknown instruction throws exception
+                () -> m.getLabels().addLabel("test", 1));
     }
 
     @org.junit.jupiter.api.Test
     public void testLabelHasWhitespaceCharacters() {
-        // TODO: test unknown instruction throws exception
+        // Create a Machine to execute the instructions.
+        Machine m = new Machine(new Registers());
+        // Create a Translator
+        Translator t = new Translator("./test/resources/programWithIllegalWhitespaceInstruction.txt");
+        // Test
+        assertThrows(RuntimeException.class, () -> t.readAndTranslate(m.getLabels(), m.getProgram()));
     }
 
     @org.junit.jupiter.api.Test
-    public void testLegalProgram() {
-        Translator t = new Translator("./test/resources/programWithLegalInstructions.txt");
+    public void testLegalProgramWorks() {
+        // Create a Machine to execute the instructions.
         Machine m = new Machine(new Registers());
+        // Create a Translator
+        Translator t = new Translator("./test/resources/programWithLegalInstructions.txt");
+
         try {
             t.readAndTranslate(m.getLabels(), m.getProgram());
             m.execute();
@@ -271,10 +278,10 @@ class Test {
         registers.set(sourceRegisterName, 5);
         // Create Subtract Instruction
         Instruction subtractInstruction = new sml.instruction.SubtractInstruction(
-                "", resultRegisterName, sourceRegisterName
+                null, resultRegisterName, sourceRegisterName
         );
         Instruction subtractInstruction2 = new sml.instruction.SubtractInstruction(
-                "", resultRegisterName, sourceRegisterName
+                null, resultRegisterName, sourceRegisterName
         );
         // Test
         assertEquals(subtractInstruction, subtractInstruction2);
@@ -294,13 +301,67 @@ class Test {
         registers.set(sourceRegisterName, 5);
         // Create Subtract Instruction
         Instruction subtractInstruction = new sml.instruction.SubtractInstruction(
-                "", resultRegisterName, sourceRegisterName
+                null, resultRegisterName, sourceRegisterName
         );
         Instruction addInstruction = new sml.instruction.AddInstruction(
-                "", resultRegisterName, sourceRegisterName
+                null, resultRegisterName, sourceRegisterName
         );
         // Test
         assertNotEquals(addInstruction, subtractInstruction);
     }
+
+    @org.junit.jupiter.api.Test
+    public void testAddInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Registers.Register sourceRegisterName = Registers.Register.ECX;
+        Instruction addInstruction = new sml.instruction.AddInstruction(null, resultRegisterName, sourceRegisterName);
+        assertEquals("add EAX ECX", addInstruction.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDivideInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Registers.Register sourceRegisterName = Registers.Register.ECX;
+        Instruction divideInstruction = new sml.instruction.DivideInstruction(null, resultRegisterName, sourceRegisterName);
+        assertEquals("div EAX ECX", divideInstruction.toString());
+    }
+    @org.junit.jupiter.api.Test
+    public void testJNZInstructionToString() {
+        Registers.Register sourceRegisterName = Registers.Register.ECX;
+        Instruction jnzInstruction = new sml.instruction.JumpIfNotZeroInstruction(null, sourceRegisterName, "test");
+        assertEquals("jnz ECX test", jnzInstruction.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMoveInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Instruction moveInstruction = new sml.instruction.MoveInstruction(null, resultRegisterName, 8);
+        assertEquals("mov EAX 8", moveInstruction.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMultiplyInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Registers.Register sourceRegisterName = Registers.Register.ECX;
+        Instruction multiplyInstruction = new sml.instruction.MultiplyInstruction(null, resultRegisterName, sourceRegisterName);
+        assertEquals("mul EAX ECX", multiplyInstruction.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testPrintInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Instruction printInstruction = new sml.instruction.PrintInstruction(null, resultRegisterName);
+        assertEquals("out EAX", printInstruction.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSubtractInstructionToString() {
+        Registers.Register resultRegisterName = Registers.Register.EAX;
+        Registers.Register sourceRegisterName = Registers.Register.ECX;
+        Instruction subtractInstruction = new sml.instruction.SubtractInstruction(null, resultRegisterName, sourceRegisterName);
+        assertEquals("sub EAX ECX", subtractInstruction.toString());
+    }
+
+
 }
 
